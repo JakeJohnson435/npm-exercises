2fb0c224f8b6468321d03707f72f640ae15b69bb
Jake J <jakejohnson818@gmail.com>
2018-04-24 11:56:11 -0500

    Finished Modules exercise

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 const $ = require('jquery');
 
+const nice = require("./say-hello.js");
+
 const sayHello = () => {
     console.log("Hello");
 };
 
 sayHello();
 
+nice.nice();
+
+// nice.rude();
 
 $("body").css("background-color", "blue");
---
+++ src/say-hello.js
@@ -0,0 +1,7 @@
+export const nice = () => {
+    console.log("Lookin fly")
+};
+
+export const rude = () =>  {
+    console.log("Brian sucks")
+};
